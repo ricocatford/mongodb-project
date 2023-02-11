db00b9f13e5640d7c0b11dbd7133f7176445ddf1
ricocatford <s7funoob@gmail.com>
2023-02-11 20:53:27 +0000

    Added first Mongo project with UI

---
+++ mongo_project.py
@@ -0,0 +1,96 @@
+import os
+import pymongo
+if(os.path.exists("env.py")):
+    import env
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "primalDB"
+COLLECTION = "celebrities"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected.")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+def show_menu():
+    print("")
+    print("1. Add a record")
+    print("2. Find a record (by name)")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
+
+    option = input("Enter option: ")
+    return option
+
+
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({"first": first.lower(), "last": last.lower()})
+    except:
+        print("Error accessing database")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
+
+
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_color = input("Enter hair color > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {
+        "first": first.lower(),
+        "last": last.lower(),
+        "dob": dob.lower(),
+        "gender": gender.lower(),
+        "hair_color": hair_color.lower(),
+        "occupation": occupation.lower(),
+        "nationality": nationality.lower(),
+    }
+
+    try:
+        coll.insert_one(new_doc)
+        print("")
+        print("Entry inserted")
+    except:
+        print("Error accessing the database")
+
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            add_record()
+        elif option == "2":
+            print("You have selected option 2")
+        elif option == "3":
+            print("You have selected option 3")
+        elif option == "4":
+            print("You have selected option 4")
+        elif option == "5":
+            conn.close()
+            break
+        else:
+            print("Invalid option")
+            print("")
+
+conn = mongo_connect(MONGO_URI)
+coll = conn[DATABASE][COLLECTION]
+main_loop()
