5d3765e53cd9acbc9d7635a1deb71ed39ae03a3c
ricocatford <s7funoob@gmail.com>
2023-02-11 20:28:25 +0000

    Update full lesson. CRUD methods for MongoDB.

--- mongo.py
+++ mongo.py
@@ -20,6 +20,59 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
+""" Insert a single document """
+# new_doc = {
+#     "first": "douglas",
+#     "last": "adams",
+#     "dob": "11/03/1952",
+#     "gender": "m",
+#     "hair_color": "grey",
+#     "occupation": "writer",
+#     "nationality": "british"
+# }
+# coll.insert(new_doc)
+
+""" Insert multipe documents """
+# new_docs = [{
+#     "first": "terry",
+#     "last": "pratchett",
+#     "dob": "28/04/1948",
+#     "gender": "m",
+#     "hair_color": "not much",
+#     "occupation": "writer",
+#     "nationality": "british"
+# }, {
+#     "first": "george",
+#     "last": "rr martin",
+#     "dob": "20/09/1948",
+#     "gender": "m",
+#     "hair_color": "white",
+#     "occupation": "writer",
+#     "nationality": "american"
+# }]
+# coll.insert_many(new_docs)
+
+""" Find documents with 'first' name set to 'douglas' """
+# documents = coll.find({"first": "douglas"})
+
+""" Delete documents with 'first' name set to 'douglas' """
+# coll.remove({"first": "douglas"})
+# documents = coll.find()
+
+""" Update a single document (first one only) """
+# coll.update_one(
+#     {"nationality": "american"},
+#     {"$set": {"hair_color": "maroon"}}
+# )
+# documents = coll.find({"nationality": "american"})
+
+""" Update all documents """
+# coll.update_many(
+#     {"nationality": "american"},
+#     {"$set": {"hair_color": "maroon"}}
+# )
+# documents = coll.find({"nationality": "american"})
+
 documents = coll.find()
 
 for doc in documents:
